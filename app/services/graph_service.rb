6ea75f6992d51a5a2a028ec6d2b564e4474f6765
mhr202 <hasnatraza202@gmail.com>
2023-01-20 23:25:58 +0500

    feat: Initial Services Setup

---
+++ app/services/graph_service.rb
@@ -0,0 +1,13 @@
+# app/services/rickymorty_service.rb
+class GraphService < ApplicationService
+  require 'json'
+  require 'rest-client'
+
+  def initialize()
+
+  end
+
+  def perform
+    
+  end
+end
